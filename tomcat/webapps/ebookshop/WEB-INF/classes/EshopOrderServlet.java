b4259601728ab63ff1f09f477a8fffac785ddd33
Cyper <86772409+BackChu@users.noreply.github.com>
2026-03-16 04:40:31 +0800

    Finished Input Verification & Extra cleanup
    Comments for some sections & finished logic for customer's info

--- tomcat/webapps/ebookshop/WEB-INF/classes/EshopOrderServlet.java
+++ tomcat/webapps/ebookshop/WEB-INF/classes/EshopOrderServlet.java
@@ -13,12 +13,15 @@ public class EshopOrderServlet extends HttpServlet {
 
    // The doGet() runs once per HTTP GET request to this servlet.
    @Override
-   public void doPost(HttpServletRequest request, HttpServletResponse response)
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
                throws ServletException, IOException {
       String action = request.getParameter("action");
       String[] ids = request.getParameterValues("id");
       HttpSession session = request.getSession();
-                  
+      // Get a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+
+      //If choose add to cart
       if ("add_to_cart".equals(action)) {
          @SuppressWarnings("unchecked")
          List<Integer> cart = (List<Integer>) session.getAttribute("cart");
@@ -41,6 +44,7 @@ public class EshopOrderServlet extends HttpServlet {
          return;
       }
 
+      //If finished and customer ready to purchase
       if ("purchase_cart".equals(action) && (ids == null || ids.length == 0)) {
          @SuppressWarnings("unchecked")
          List<Integer> cart = (List<Integer>) session.getAttribute("cart");
@@ -52,13 +56,14 @@ public class EshopOrderServlet extends HttpServlet {
          }
       }
 
+      //Retrieving customer's info
       String cust_name = request.getParameter("cust_name");
       String cust_email = request.getParameter("cust_email");
       String cust_phone = request.getParameter("cust_phone");
 
+      //If customer did not choose a book/empty cart
       if (ids == null || ids.length == 0) {
          response.setContentType("text/html");
-         PrintWriter out = response.getWriter();
          out.println("<!DOCTYPE html>");
          out.println("<html>");
          out.println("<head><title>Order</title></head>");
@@ -69,35 +74,38 @@ public class EshopOrderServlet extends HttpServlet {
          return;
       }
 
+      //Initial Boxes for customer's info
       if (cust_name == null || cust_email == null || cust_phone == null) {
-         response.setContentType("text/html");
-         PrintWriter out = response.getWriter();
-         out.println("<!DOCTYPE html>");
-         out.println("<html>");
-         out.println("<head><title>Customer Details</title></head>");
-         out.println("<body>");
-         out.println("<h3>Please enter your details to confirm order</h3>");
-         out.println("<form method='get' action='eshoporder'>");
-         if (action != null) {
-            out.println("<input type='hidden' name='action' value='" + action + "' />");
-         }
-         for (String id : ids) {
-            out.println("<input type='hidden' name='id' value='" + id + "' />");
-         }
-         out.println("<p>Enter your Name: <input type='text' name='cust_name' required /></p>");
-         out.println("<p>Enter your Email: <input type='text' placeholder='abcd@domain.com' name='cust_email' required /></p>");
-         out.println("<p>Enter your Phone Number: <input type='text' placeholder='12345678' name='cust_phone' required /></p>");
-         out.println("<p><input type='submit' value='CONFIRM ORDER' /></p>");
-         out.println("</form>");
-         out.println("</body></html>");
-         out.close();
-         return;
+            response.setContentType("text/html");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head><title>Customer Details</title></head>");
+            out.println("<body>");
+            out.println("<h3>Please enter your details to confirm order</h3>");
+            out.println("<form method='get' action='eshoporder'>");
+            if (action != null) {
+               out.println("<input type='hidden' name='action' value='" + action + "' />");
+            }
+            for (String id : ids) {
+               out.println("<input type='hidden' name='id' value='" + id + "' />");
+            }
+            out.println("<p>Enter your Name: <input type='text' name='cust_name' required /></p>");
+            out.println("<p>Enter your Email: <input type='text' placeholder='abcd@domain.com' name='cust_email' required /></p>");
+            out.println("<p>Enter your Phone Number: <input type='text' placeholder='12345678' name='cust_phone' required /></p>");
+            out.println("<p><input type='submit' value='CONFIRM ORDER' /></p>");
+            out.println("</form>");
+            out.println("</body></html>");
+            out.close();
+            return;
+      }
+
+      //Incorrect email/phone
+      if (!isValidEmail(cust_email) || cust_phone.length() != 8) {
+         out.println("<h3>Please return and correct your email and/or phone number</h3>");
       }
 
       // Set the MIME type for the response message
       response.setContentType("text/html");
-      // Get a output writer to write the response message into the network socket
-      PrintWriter out = response.getWriter();
       // Print an HTML page as the output of the query
       out.println("<!DOCTYPE html>");
       out.println("<html>");
@@ -115,44 +123,37 @@ public class EshopOrderServlet extends HttpServlet {
          Statement stmt = conn.createStatement();
       ) {
          // Step 3 & 4: Execute a SQL SELECT query and Process the query result
-         // Retrieve the books' id. Can order more than one books.
-         if (ids != null) {
-            if (true/*cust_name.isEmpty() && isValidEmail(cust_email) && cust_phone.length() == 8*/) {
-               String sqlStr;
-               int count;
-   
-               // Process each of the books
-               for (int i = 0; i < ids.length; ++i) {
-                  // Update the qty of the table books
-                  sqlStr = "UPDATE books SET qty = qty - 1 WHERE id = " + ids[i];
-                  out.println("<p>" + sqlStr + "</p>");  // for debugging
-                  count = stmt.executeUpdate(sqlStr);
-                  out.println("<p>" + count + " record updated.</p>");
-   
-                  // Create a transaction record
-                  sqlStr = "INSERT INTO order_records (id, qty_ordered, cust_name, cust_email, cust_phone) VALUES ("
-                        + ids[i] + ", 1, '" + cust_name + "', '" + cust_email + "', '" + cust_phone + "')";
-                  out.println("<p>" + sqlStr + "</p>");  // for debugging
-                  count = stmt.executeUpdate(sqlStr);
-                  out.println("<p>" + count + " record inserted.</p>");
-                  out.println("<h3>Your order for book id=" + ids[i]
-                        + " has been confirmed.</h3>");
-               }
+         //If all the information are filled correctly
+         if (cust_name != null && isValidEmail(cust_email) && cust_phone.length() == 8){
+            String sqlStr;
+            int count;
 
-               if ("purchase_cart".equals(action)) {
-                  @SuppressWarnings("unchecked")
-                  List<Integer> cart = (List<Integer>) session.getAttribute("cart");
-                  if (cart != null) {
-                     cart.clear();
-                  }
-               }
-               out.println("<h3>Thank you.<h3>");
-            } else { //Incorrect email/phone input
-               out.println("<h3>Please input your name and/or correct email and/or phone number</h3>");
+            // Process each of the books
+            for (int i = 0; i < ids.length; ++i) {
+               // Update the qty of the table books
+               sqlStr = "UPDATE books SET qty = qty - 1 WHERE id = " + ids[i];
+               out.println("<p>" + sqlStr + "</p>");  // for debugging
+               count = stmt.executeUpdate(sqlStr);
+               out.println("<p>" + count + " record updated.</p>");
+
+               // Create a transaction record
+               sqlStr = "INSERT INTO order_records (id, qty_ordered, cust_name, cust_email, cust_phone) VALUES ("
+                     + ids[i] + ", 1, '" + cust_name + "', '" + cust_email + "', '" + cust_phone + "')";
+               out.println("<p>" + sqlStr + "</p>");  // for debugging
+               count = stmt.executeUpdate(sqlStr);
+               out.println("<p>" + count + " record inserted.</p>");
+               out.println("<h3>Your order for book id=" + ids[i]
+                     + " has been confirmed.</h3>");
             }
 
-         } else { // No book selected
-            out.println("<h3>Please go back and select a book...</h3>");
+            if ("purchase_cart".equals(action)) {
+               @SuppressWarnings("unchecked")
+               List<Integer> cart = (List<Integer>) session.getAttribute("cart");
+               if (cart != null) {
+                  cart.clear();
+               }
+            }
+            out.println("<h3>Thank you.<h3>");
          }
          // === Step 4 ends HERE - Do NOT delete the following codes ===
       } catch(SQLException ex) {
